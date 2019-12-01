4e9ae9ffdeb93863c096113abd65ea51e71d5793
Jaeger <46061258+JaegerTao@users.noreply.github.com>
2019-12-01 20:48:44 +0800

    增加Vue前端基础配置
    增加vue配置，准备开发页面

---
+++ GeekMeWeb/src/App.vue
@@ -0,0 +1,28 @@
+<template>
+  <div id="app">
+    <img alt="GeekMelogo" src="./assets/logo.png">
+    <HelloWorld msg="GeekMe start"/>
+  </div>
+</template>
+
+<script>
+import HelloWorld from './components/header.vue'
+
+export default {
+  name: 'app',
+  components: {
+    header
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
